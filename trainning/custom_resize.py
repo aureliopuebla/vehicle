687381c10ae79a4aea84919b05a2b31ce8833873
Chaveze <chaaveze@gmail.com>
2019-01-26 18:49:06 -0600

    Adds source PavoReal resized

--- trainning/custom_resize.py
+++ trainning/custom_resize.py
@@ -55,17 +55,31 @@ def main(argv):
 	for img_path in list_of_images_paths:
 		# Load img
 		cur_img = cv2.imread(path_to_source_folder + img_path, cv2.IMREAD_COLOR)
-		
-		# load with and height
+		print ('Current img size : ', cur_img.shape[:2])
+		resize_img = []
+
+		# load height width
 		im_row, im_col = cur_img.shape[:2] # rows, colums
+		ratio = im_row/im_col
 		
-		print ('image dim: ', im_row*im_col, 'image ratio: ', im_row/im_col)
+		print ('image dim: ', im_row*im_col, 'image ratio: ', ratio)
 		if (im_row*im_col < 800*600):
 			print ('image OK.')
+			cv2.imwrite(path_to_out_folder + img_path, cur_img)
+		else:
+			# Portrait
+			if (ratio > 1):
+				fac_x = float(800/im_col)
+				resize_img = cv2.resize(cur_img,None,fx=fac_x, fy=fac_x)
+			# Landscape
+			else:
+				fac_y = float(800/im_row)
+				resize_img = cv2.resize(cur_img,None,fx=fac_y, fy=fac_y)
+		
 
-		# print(img_path)
-
-
+			print ('Saving img w/ new shape ', resize_img.shape[:2])
+			cv2.imwrite(path_to_out_folder + img_path, resize_img)
+			print('saved\n')
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
