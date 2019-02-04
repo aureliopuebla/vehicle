41f89416760b9edc18e533e778a714cf7d0edbf1
aureliopuebla <aureliopuebla_9@hotmail.com>
2019-02-04 16:12:00 -0600

    Modified stereo node to include camera_info and optical frame reference.

--- src/stereo.cpp
+++ src/stereo.cpp
@@ -1,5 +1,9 @@
 #include <ros/ros.h>
+#include <image_transport/image_transport.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <time.h>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
@@ -12,16 +16,44 @@
 #define FIRST_IMAGE_IDX 5
 #define LAST_IMAGE_IDX 334
 #define DEPTH_TO_DISP 128*65536
+#define FRAME_ID "multisense/left_camera_optical_frame"
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "stereo");
   ros::NodeHandle nh;
-  ros::Publisher leftPub = nh.advertise<sensor_msgs::Image>("camera/image", 1);
-  ros::Publisher dispPub = nh.advertise<sensor_msgs::Image>("camera/disp", 1);
+
+  image_transport::ImageTransport it(nh);
+  image_transport::CameraPublisher leftPub =
+      it.advertiseCamera("camera/left/image", 1);
+  image_transport::CameraPublisher depthPub =
+      it.advertiseCamera("camera/depth/image", 1);
+  image_transport::CameraPublisher dispPub =
+      it.advertiseCamera("camera/disp/image", 1);
+
+  sensor_msgs::Image leftImageMsg, depthImageMsg, dispImageMsg;
+  // Camera Parameters extracted from calib_cam_to_cam.txt
+  sensor_msgs::CameraInfo camInfoMsg;
+  camInfoMsg.header.frame_id = FRAME_ID;
+  camInfoMsg.height = 1242;
+  camInfoMsg.width = 375;
+  /*camInfoMsg.distortion_model = sensor_msgs::distortion_models::PLUMB_BOB;
+  camInfoMsg.D = {
+      -3.691481e-01, 1.968681e-01, 1.353473e-03, 5.677587e-04, -6.770705e-02};*/
+  camInfoMsg.K = {9.597910e+02, 0.000000e+00, 6.960217e+02,
+                  0.000000e+00, 9.569251e+02, 2.241806e+02,
+                  0.000000e+00, 0.000000e+00, 1.000000e+00};
+  camInfoMsg.R = {9.998817e-01, 1.511453e-02, -2.841595e-03,
+                  -1.511724e-02, 9.998853e-01, -9.338510e-04,
+                  2.827154e-03, 9.766976e-04, 9.999955e-01};
+  camInfoMsg.P = {7.215377e+02, 0.000000e+00, 6.095593e+02, 4.485728e+01,
+                  0.000000e+00, 7.215377e+02, 1.728540e+02, 2.163791e-01,
+                  0.000000e+00, 0.000000e+00, 1.000000e+00, 2.745884e-03};
 
   ros::Rate loop_rate(5);
   while (nh.ok()) {
     for (int i = FIRST_IMAGE_IDX; nh.ok() && i <= LAST_IMAGE_IDX; i++) {
+      ros::Time currentTimestamp = ros::Time::now();
+
       std::stringstream ssl, ssd;
       ssl << DATA_DIR << LEFT_DIR; 
       ssl << std::setfill('0') << std::setw(10) << i << ".png";
@@ -29,13 +61,12 @@ int main(int argc, char** argv) {
       ssd << std::setfill('0') << std::setw(10) << i << ".png";
 
       cv::Mat leftImage = cv::imread(ssl.str(), cv::IMREAD_UNCHANGED);
-      // Temporary hold Depth Image.
-      cv::Mat dispImage = cv::imread(ssd.str(), cv::IMREAD_UNCHANGED);
-      cv::threshold(DEPTH_TO_DISP / (dispImage + 1), // Convert to Disparity.
+      cv::Mat depthImage = cv::imread(ssd.str(), cv::IMREAD_UNCHANGED);
+      /*cv::threshold(DEPTH_TO_DISP / (dispImage + 1), // Convert to Disparity.
                     dispImage, // dst
                     65535-1, // thresshold, eliminates invalid depths/disps.
                     0, 
-                    cv::THRESH_TOZERO_INV);
+                    cv::THRESH_TOZERO_INV);  !!!!!!!!!!!!!!!!!!!! */
       // Extend sparse values to simulate the Multisense density.
       //cv::Mat count = (dispImage != 0) / 255;
       //count.convertTo(count, CV_16UC1);
@@ -44,13 +75,14 @@ int main(int argc, char** argv) {
       //cv::blur(dispImage, dispImage, cv::Size(3, 3));
       //dispImage = dispImage.mul(count);
 
-      sensor_msgs::ImagePtr leftMsg = cv_bridge::CvImage(
-          std_msgs::Header(), "bgr8", leftImage).toImageMsg();
-      sensor_msgs::ImagePtr dispMsg = cv_bridge::CvImage(
-          std_msgs::Header(), "16UC1", dispImage).toImageMsg();
-    
-      leftPub.publish(leftMsg);
-      dispPub.publish(dispMsg);
+      cv_bridge::CvImage(camInfoMsg.header, "bgr8", leftImage)
+          .toImageMsg(leftImageMsg);
+      cv_bridge::CvImage(camInfoMsg.header, "16UC1", depthImage)
+          .toImageMsg(depthImageMsg);
+
+      leftPub.publish(leftImageMsg, camInfoMsg, currentTimestamp);
+      depthPub.publish(depthImageMsg, camInfoMsg, currentTimestamp);
+
       ros::spinOnce();
       loop_rate.sleep();
     }
